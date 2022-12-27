ed38c1e08fa97f8c2bbb4d4049750a62dba1ab9a
jo! <me@j0.lol>
2022-12-27 19:03:15 +0000

    among us

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
-  description = "REST API for any Postgres database";
+	# thanks https://blog.sekun.net/posts/packaging-prebuilt-binaries-with-nix/
+
+  description = "A standalone 2-10 player remake of the Mario vs Luigi gamemode from New Super Mario Bros DS.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
@@ -10,15 +12,47 @@
       with import nixpkgs { system = "x86_64-linux"; };
 
       stdenv.mkDerivation rec {
-        name = "postgrest-${version}";
+        name = "mariovsluigi-${version}";
 
         version = "1.7.1.0-beta";
 
-		src = pkgs.fetchurl {
-		  # Remember `rec`!
-		  url = "https://github.com/ipodtouch0218/NSMB-MarioVsLuigi/releases/download/v${version}/MarioVsLuigi-Linux-v${version}.zip";
-		  sha256 = "";
-		};
+				src = pkgs.fetchurl {
+				  # Remember `rec`!
+				  url = "https://github.com/ipodtouch0218/NSMB-MarioVsLuigi/releases/download/v${version}/MarioVsLuigi-Linux-v${version}.zip";
+				  sha256 = "sha256-rb9vZ3w4BfzygsdOCPLVX87T5tBXk6Aw+ba9I12fhwQ=";
+				};
+
+				sourceRoot = ".";
+
+				nativeBuildInputs = [
+					unzip
+					autoPatchelfHook
+				];
+
+				buildInputs = [
+					zlib
+				];
+
+				installPhase = ''
+					ls MarioVsLuigi-Linux/
+					mkdir -p $out/{bin,share}/
+					cp -r MarioVsLuigi-Linux/linux_Data $out/share
+					install -m755 MarioVsLuigi-Linux/UnityPlayer.so $out/share
+					install -m755 MarioVsLuigi-Linux/linux.x86_64 $out/share
+					echo "cd $out/share/ && steam-run ./linux.x86_64" > $out/bin/mariovsluigi
+					chmod 755 $out/bin/mariovsluigi
+				'';
+
+			  desktopItems = [
+			    (makeDesktopItem {
+			      name = "Mario VS Luigi";
+			      icon = "mariovsluigi";
+			      exec = "mariovsluigi";
+			      genericName = "Mario VS Luigi";
+			      desktopName = "Mario VS Luigi";
+			      categories = [ "Game" ];
+			    })
+			  ];
 
       };
   };
